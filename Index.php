af8c3002a1b4b88c08f92e504587e1891bde69be
Jessica Zheng <jessicazh0103@gmail.com>
2026-06-13 16:53:53 -0500

    Add execution date to footer

--- Index.php
+++ Index.php
@@ -74,6 +74,7 @@ use App\Utilidades\Navegacion;
     <p>Universidad Tecnológica de Panamá</p>
     <p>Licenciatura en Desarrollo & Gestión de Software - Desarrollo de Software VII</p>
     <p>Erick Hou &nbsp;·&nbsp; Genesis Luo &nbsp;·&nbsp; Jessica Zheng</p>
+    <p>Fecha de ejecución: <?= date("d/m/Y") ?></p>
     <p>&copy; 2026 Todos los derechos reservados.</p>
 </footer>
 
